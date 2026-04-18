2ddae7398595f3cdc7c87d6daa446cb87ccb900f
Anna Elsukova <86211452+AnnaEls@users.noreply.github.com>
2026-04-18 14:01:14 +0200

    Create Training.py

---
+++ Training/Training.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn.functional as F
+
+def train_model(model, input, path, learning_rate=1e-3, num_iter=1, patch_size=1, mask_ratio=0.2, show_image=False, seed=42):    
+    
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = model.to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    input = input.to(device)
+    model.train()
+
+    loss_history = []
+
+    os.makedirs(path, exist_ok=True)
+
+    for it in range(num_iter):   
+        # deterministic mask that changes by epoch
+        masked_input, mask = random_patch_mask(
+            input,
+            patch_size=patch_size,
+            mask_ratio=mask_ratio,
+            seed=seed,         
+            epoch=it            
+        )
+
+        output_afno = model(masked_input)
+
+        loss = F.mse_loss(output_afno * (1 - mask), input * (1 - mask))
+        loss_history.append(loss.item())
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        
+    np.save(os.path.join(path, 'loss_history.npy'), np.array(loss_history))
