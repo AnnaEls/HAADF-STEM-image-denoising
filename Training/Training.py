1ebdf1de4669b77afd39cf0a5fa246ec23b85f48
Anna Elsukova <86211452+AnnaEls@users.noreply.github.com>
2026-04-30 14:25:35 +0200

    Update Training.py

--- Training/Training.py
+++ Training/Training.py
@@ -56,7 +56,7 @@ def train_model_with_prior(model, input, path, learning_rate=1e-3, learning_rate
     model = model.to(device) #reconstruction model
     input = input.to(device)
     #z = input.clone().detach().requires_grad_(True) #prior
-    z = torch.zeros_like(input_norm, requires_grad=True)
+    z = torch.zeros_like(input, requires_grad=True)
     optimizer = torch.optim.Adam([{"params": model.parameters(), "lr": lr_model},
                                   {"params": [z], "lr": lr_z}])
    
