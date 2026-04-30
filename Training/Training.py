0ba4ed190e9f71605a93c0b58d39a2eaa6283302
Anna Elsukova <86211452+AnnaEls@users.noreply.github.com>
2026-04-30 13:31:02 +0200

    Update Training.py

--- Training/Training.py
+++ Training/Training.py
@@ -50,7 +50,7 @@ def train_model(model, input, path, learning_rate=1e-3, num_iter=1, patch_size=1
         model.train()
     np.save(os.path.join(path, 'loss_history.npy'), np.array(loss_history))    
 
-def train_model_with_prior(model, input, path, learning_rate=1e-3, learning_rate_prior = 1e-3, num_iter=1, patch_size=1, mask_ratio=0.2, show_image=False, seed=42):
+def train_model_with_prior(model, input, path, learning_rate=1e-3, learning_rate_prior = 1e-3, sigma = 0.6, reg = 0.1, num_iter=1, patch_size=1, mask_ratio=0.2, show_image=False, seed=42):
     
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")    
     model = model.to(device) #reconstruction model
@@ -75,7 +75,12 @@ def train_model_with_prior(model, input, path, learning_rate=1e-3, learning_rate
 
         output = model(masked_input)
 
-        loss = F.mse_loss(output * (1 - mask), input * (1 - mask))
+        eps = sigma * torch.randn_like(z) #disturbance
+        output_distrubed = model(z + eps)
+
+        reconstruction_loss = F.mse_loss(output * (1 - mask), input * (1 - mask))
+        reg_loss = F.mse_loss(output_distrubed, z)
+        loss = reconstruction_loss + reg*reg_loss
         loss_history.append(loss.item())
 
         optimizer.zero_grad()
@@ -87,7 +92,7 @@ def train_model_with_prior(model, input, path, learning_rate=1e-3, learning_rate
             denoised_image= model(input)
             tifffile.imwrite(f'{path}/{it+1:04d}.tif', convert(denoised_image.squeeze().detach().cpu().numpy()), imagej=True)
             if show_image:
-               print(f"epoch {it + 1}, loss={loss.item():.6f}")
+               print(f"epoch {it + 1}, reconstruction loss={reconstruction_loss.item():.6f}, regularization loss={reg_loss.item():.6f}")
                plt.imshow(denoised_image.squeeze().detach().cpu().numpy(), cmap='gray'); plt.axis('off'); plt.tight_layout();
                plt.show()
         model.train()
