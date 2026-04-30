792c8bbe0e480fd5bcef005509168a6a271d5ac1
Anna Elsukova <86211452+AnnaEls@users.noreply.github.com>
2026-04-30 13:18:11 +0200

    Update Training.py

--- Training/Training.py
+++ Training/Training.py
@@ -50,49 +50,49 @@ def train_model(model, input, path, learning_rate=1e-3, num_iter=1, patch_size=1
         model.train()
     np.save(os.path.join(path, 'loss_history.npy'), np.array(loss_history))    
 
+def train_model_with_prior(model, input, path, learning_rate=1e-3, learning_rate_prior = 1e-3, num_iter=1, patch_size=1, mask_ratio=0.2, show_image=False, seed=42):
+    
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")    
+    model = model.to(device) #reconstruction model
+    input = input.to(device)
+    z = input.clone().detach().requires_grad_(True) #prior
+    optimizer = torch.optim.Adam([{"params": model.parameters(), "lr": learning_rate},
+                                  {"params": [z], "lr": learning_rate_prior}])
+    model.train()
 
-def train_model_self_guided(model, input, path, sigma, reg_coef, learning_rate=1e-3, num_iter=1, patch_size=1, mask_ratio=0.2, show_image=False, seed=42): 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu") 
-    model = model.to(device) 
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate) 
-    input = input.to(device) 
-    input_1 = input 
-    input_2 = input 
-    
-    model.train() 
-    
-    loss_history = [] 
-    
-    os.makedirs(path, exist_ok=True) 
-    for it in range(num_iter): 
-        input_1 = input + torch.rand(1).to(device) * torch.randn_like(input) 
-        input_2 = input + torch.rand(1).to(device) * torch.randn_like(input) 
+    loss_history = []
 
-        input_1 = (input_1 - input_1.mean())/(input_1.std())
-        input_2 = (input_2 - input_2.mean())/(input_2.std())
+    os.makedirs(path, exist_ok=True)
+
+    for it in range(num_iter):   
+        masked_input, mask = random_patch_mask(
+            z,
+            patch_size=patch_size,
+            mask_ratio=mask_ratio,
+            seed=seed,         
+            epoch=it            
+        )
+
+        output = model(masked_input)
+
+        loss = F.mse_loss(output * (1 - mask), input * (1 - mask))
+        loss_history.append(loss.item())
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
         
-        masked_input_1, mask = random_patch_mask( input, patch_size=patch_size, mask_ratio=mask_ratio, seed=seed, epoch=it ) 
-        masked_input_2, mask = random_patch_mask( input_2, patch_size=patch_size, mask_ratio=mask_ratio, seed=seed, epoch=it ) 
-        
-        output_1 = model(input_1) 
-        output_2 = model(input_2) 
-        
-        loss_recon = F.mse_loss(output_1 , input) 
-        loss_reg = torch.mean((output_1 - output_2.detach())**2) 
-        loss = loss_recon + reg_coef*loss_reg
-        
-        loss_history.append(loss.item()) 
-        optimizer.zero_grad() 
-        
-        loss.backward() 
-        optimizer.step() 
-        model.eval() 
-        
+        model.eval()
         with torch.no_grad():
-            denoised_image= model(input) 
-            tifffile.imwrite(f'{path}/{it+1:04d}.tif', convert(denoised_image.squeeze().detach().cpu().numpy()), imagej=True) 
+            denoised_image= model(input)
+            tifffile.imwrite(f'{path}/{it+1:04d}.tif', convert(denoised_image.squeeze().detach().cpu().numpy()), imagej=True)
             if show_image:
-                print(f"epoch {it + 1}, loss_recon={loss_recon.item():.6f}, loss_reg ={loss_reg.item():.6f}") 
-                plt.imshow(denoised_image.squeeze().detach().cpu().numpy(), cmap='gray'); plt.axis('off'); plt.tight_layout(); plt.show() 
-        model.train() 
-    np.save(os.path.join(path, 'loss_history.npy'), np.array(loss_history))       
+               print(f"epoch {it + 1}, loss={loss.item():.6f}")
+               plt.imshow(denoised_image.squeeze().detach().cpu().numpy(), cmap='gray'); plt.axis('off'); plt.tight_layout();
+               plt.show()
+        model.train()
+    np.save(os.path.join(path, 'loss_history.npy'), np.array(loss_history)) 
+
+    
+    
+   
