a7155621feed9c02c564688f77e91044d0648568
Anna Elsukova <86211452+AnnaEls@users.noreply.github.com>
2026-06-02 22:51:49 +0200

    Update Training.py

--- Training/Training.py
+++ Training/Training.py
@@ -55,5 +55,45 @@ def train_model(model, input, path, learning_rate=1e-3, num_iter=1, patch_size=1
     np.save(os.path.join(path, 'loss_history.npy'), np.array(loss_history))    
 
    
+ 
+def train_model_amp_phase(model, input, path, learning_rate=1e-3, num_iter=1, patch_size=1, mask_ratio=0.2, show_image=False, seed=42):    
     
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = model.to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    input = input.to(device)
+    model.train()
+
+    loss_history = []
+
+    os.makedirs(path, exist_ok=True)
+
+    for it in range(num_iter):   
+        masked_input, mask = random_patch_mask(
+            input,
+            patch_size=patch_size,
+            mask_ratio=mask_ratio,
+            seed=seed,         
+            epoch=it            
+        )
+
+        output_afno = model(masked_input)
+
+        loss = F.mse_loss(output_afno * (1 - mask), input * (1 - mask))
+        loss_history.append(loss.item())
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        model.eval()
+        with torch.no_grad():
+            denoised_image= model(input)[0:1, :, :, :]
+            tifffile.imwrite(f'{path}/{it+1:04d}.tif', convert(denoised_image.squeeze().detach().cpu().numpy()), imagej=True)
+            if show_image:
+               print(f"epoch {it + 1}, loss={loss.item():.6f}")
+               plt.imshow(denoised_image.squeeze().detach().cpu().numpy(), cmap='gray'); plt.axis('off'); plt.tight_layout();
+               plt.show()
+        model.train()
+    np.save(os.path.join(path, 'loss_history.npy'), np.array(loss_history))       
    
