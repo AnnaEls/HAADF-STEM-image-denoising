b33c8e1100dc7811a3e083391c552a7f05bc228a
Anna Elsukova <86211452+AnnaEls@users.noreply.github.com>
2026-09-18 18:45:15 +0200

    Update Training.py

--- Training/Training.py
+++ Training/Training.py
@@ -120,7 +120,7 @@ def train_hybrid_model_with_mask_offset(model, input, path, learning_rate=1e-3, 
             input,
             patch_size=patch_size,
             mask_ratio=mask_ratio,
-            offset = mask_offset,
+            max_offset = mask_offset,
             seed=seed,         
             epoch=it            
         )
